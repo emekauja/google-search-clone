7f97825125d342385904c10a826afb5a324e7654
Emeka Uja <ujadninth@gmail.com>
2021-08-18 22:56:29 +0100

    fix: passHref

--- pages/components/Pagination.js
+++ pages/components/Pagination.js
@@ -10,7 +10,8 @@ function Pagination() {
     <div className="flex justify-between max-w-lg text-blue-700 mb-10">
       {startIndex >= 10 && (
         <Link
-          href={`/search?term=${router.query.term}&start=${startIndex - 10}`}>
+          href={`/search?term=${router.query.term}&start=${startIndex - 10}`}
+          passHref>
           <div className="flex flex-grow flex-col items-center cursor-pointer hover:underline">
             <ChevronLeftIcon className="h-5" />
             <p>Previous</p>
@@ -18,7 +19,9 @@ function Pagination() {
         </Link>
       )}
 
-      <Link href={`/search?term=${router.query.term}&start=${startIndex + 10}`}>
+      <Link
+        href={`/search?term=${router.query.term}&start=${startIndex + 10}`}
+        passHref>
         <div className="flex flex-grow flex-col items-center cursor-pointer hover:underline">
           <ChevronRightIcon className="h-5" />
           <p>Next</p>
